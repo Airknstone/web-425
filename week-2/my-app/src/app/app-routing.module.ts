753b92dc44bd0cda11a63543fdff57027569b4de
Arkenstone <trejo.allan@yahoo.com>
2022-06-02 21:13:45 -0700

    updated comments

---
+++ week-2/my-app/src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+/*
+   Title: app-routing.modules.ts
+ Author: Professor Krasso
+ Date: 06/02/2022
+ Modified By: Allan Trejo
+ Description: Routing Module Set up
+
+ */
+
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
---
+++ week-2/my-app/src/app/app.component.ts
@@ -0,0 +1,18 @@
+/*
+   Title: app.component.ts
+ Author: Professor Krasso
+ Date: 06/02/2022
+ Modified By: Allan Trejo
+ Description: Root component business logic for my-app routing examples
+
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent {
+  title = 'my-app';
+}

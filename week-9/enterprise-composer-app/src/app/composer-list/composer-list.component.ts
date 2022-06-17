b359282f0774cfcce46c0377937013253863c165
Arkenstone <trejo.allan@yahoo.com>
2022-06-17 15:47:30 -0700

    Updated changes

---
+++ week-9/enterprise-composer-app/src/app/composer-list/composer-list.component.ts
@@ -0,0 +1,40 @@
+/*
+   Title: composer-list.component.ts
+ Author: Professor Krasso
+ D Date: 06/16/2022
+ Modified By: Allan Trejo
+ Description: Exercise 4.3 - Handling Events with Observables
+
+ */
+
+/* Imports */
+import { IComposer } from './../composer.interface';
+import { Component, OnInit } from '@angular/core';
+import { ComposerService } from './../composer.service';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-composer-list',
+  templateUrl: './composer-list.component.html',
+  styleUrls: [ './composer-list.component.css' ]
+})
+export class ComposerListComponent implements OnInit {
+  /* Defines Variables and initializes array */
+  composers: Observable<IComposer[]>;
+  txtSearchControl = new FormControl('');
+  constructor (private composerService: ComposerService) {
+
+    this.composers = this.composerService.getComposers();
+    this.txtSearchControl.valueChanges.pipe(debounceTime(500)).subscribe(val => this.filterComposers(val));
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  filterComposers(name: string): void {
+    this.composers = this.composerService.filterComposers(name);
+  }
+}
